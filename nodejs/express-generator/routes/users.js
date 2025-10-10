a610de54185aa91d30a15861654bf242fc529027
Vitória Behenck Alves <vic.behenck.a@gmail.com>
2025-10-10 16:53:31 -0300

    express generator

---
+++ nodejs/express-generator/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.render('users', { title: 'Jean Moreau' });
+});
+
+module.exports = router;
